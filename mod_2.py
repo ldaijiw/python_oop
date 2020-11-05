f632538553f956cf055ae7b177f010071f022427
ldaijiw <lwaltmann@spartaglobal>
2020-11-05 12:46:29 +0000

    added comments on mod 2

--- mod_2.py
+++ mod_2.py
@@ -1,4 +1,6 @@
 import mod_1
 
+# code being run in mod_2 file => name == main
 print("This is mod_2 ->", __name__)
+# since mod_1 is imported => name == mod_1
 print("This is mod_1 ->", mod_1.__name__)
